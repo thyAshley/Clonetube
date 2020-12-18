0ddbbe40c06d43eefcfdbfcaf42f5c901a94e0a9
Ashley <thy.ashley@gmail.com>
2020-12-18 23:59:20 +0800

    added layout for nav bar

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import YouTubeIcon from '@material-ui/icons/YouTube';
 import RecorderIcon from '@material-ui/icons/Reorder';
+import SearchIcon from '@material-ui/icons/Search';
+import AddIcon from '@material-ui/icons/AddBox';
 
 import './navbar.scss';
 
@@ -14,8 +16,18 @@ const Navbar: React.FC = () => {
         <YouTubeIcon className="icon youtube" />
         <h1>CloneTube</h1>
       </div>
-      <div className="center"></div>
-      <div className="right"></div>
+      <div className="center">
+        <input className="searchbar" placeholder="Search" />
+        <button className="searchbar-button">
+          <SearchIcon className="icon search" />
+        </button>
+      </div>
+      <div className="right">
+        <button className="addvideo-button">
+          <AddIcon className="icon add" />
+        </button>
+        <button className="profile"></button>
+      </div>
     </div>
   );
 };
